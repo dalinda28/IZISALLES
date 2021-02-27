280e3028ca212920d562747d23173dfed630ad68
dalinda <aloui.dalinda28@gmail.com>
2021-02-27 22:35:33 +0100

    init commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <Navbar></Navbar>
+    <img src="./assets/room1.jpg" alt="room" style="object-fit: contain; width:100%; height:20%; opacity:0.75;"/>
+    <div class="centered">
+      <h2>
+        Réservez des salles de réunion de travail et de conférence 
+      </h2>
+      <button type="button" class="btn btn-secondary btn-lg" style="margin-left:30%">Réservez !</button>
+      <button type="button" class="btn btn-light btn-lg m-3">Contactez nous</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/Navbar";
+
+export default {
+  name: "App",
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<style>
+html {
+  font-size: 1rem;
+}
+img{
+
+}
+.centered {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav class="navbar fixed-top transparent navbar-expand-lg navbar-light ">
+    <div class="container-fluid h5">
+      <a class="navbar-brand" href="#">LOGO</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Réservation</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Mes salles</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+       
+      </div>
+      <div class="ml-auto">  
+          <ul class="navbar-nav ">Compte</ul>
+       </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
